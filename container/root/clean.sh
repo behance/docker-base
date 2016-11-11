845979c50406929a56f895eef701002db689cca0
Bryan Latten <latten@adobe.com>
2016-11-11 15:57:00 -0500

    Scripts: added clean and security_updates conveniences
    - Set the terminal mode “properly”
    - Running security update as part of build

---
+++ container/root/clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+#-------------------------------------------------------------------
+# Performs cleanup, ensure unnecessary packages and package lists
+# are safely removed, without triggering Docker AUFS permission bug
+#-------------------------------------------------------------------
+
+apt-get autoclean -y && \
+apt-get autoremove -y && \
+rm -rf /var/lib/{cache,log}/ && \
+rm -rf /var/lib/apt/lists/*.lz4 && \
+rm -rf /tmp/* /var/tmp/*
---
+++ container/root/security_updates.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#-------------------------------------------------------------------
+# Upgrade just the packages listed as security, without affecting
+# any subsequent usages of apt-get
+#-------------------------------------------------------------------
+
+apt-get update && \
+grep security /etc/apt/sources.list > /tmp/security.list && \
+apt-get upgrade -oDir::Etc::Sourcelist=/tmp/security.list -yq && \
+rm /tmp/security.list

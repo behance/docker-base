0572fff7d325f5832a049b3ad79c598848708abd
Bryan Latten <latten@adobe.com>
2017-01-21 11:46:56 -0500

    Dockerfile: updated Alpine to 3.5
    - Also added Alpine clean convenience script

---
+++ container/root/scripts/clean_alpine.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+# --- Alpine variant only, for other variants, see /scripts folder
+
+#-------------------------------------------------------------------
+# Performs cleanup, ensure unnecessary packages and package lists
+# are safely removed, without triggering Docker AUFS permission bug
+#-------------------------------------------------------------------
+
+rm -rf /var/cache/apk/*

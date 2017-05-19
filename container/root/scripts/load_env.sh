1a83f4accf469900bc160e5a16116b1dc011185f
Mayank Anand <mayanand@adobe.com>
2017-05-18 22:22:22 -0700

    shell script to load the env variables in cont-init.d scripts

---
+++ container/root/scripts/load_env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+# --- For all variants
+
+#-------------------------------------------------------------------
+# Loads environment variables for cont-init.d scripts
+# Needs to be sourced when we have to use normal bash shell
+# instead of with-contenv bash shell
+#-------------------------------------------------------------------
+
+CONTAINER_ENV_LOC=/var/run/s6/container_environment/*
+
+for f in $CONTAINER_ENV_LOC; do
+  env_variable_name="${f##*/}"
+  if [ "${env_variable_name}" != "UID" ] && [ "${env_variable_name}" != "!" ] && [ "${env_variable_name}" != "_" ]; then
+    export "${env_variable_name}"="`cat $f`"
+  fi
+done

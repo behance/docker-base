a692da8477b54882498848eaf7ba24c57a8d42ec
Bryan Latten <latten@adobe.com>
2019-05-21 13:42:36 -0400

    Scripts: added -f flag for curl

--- container/root/scripts/install_goss.sh
+++ container/root/scripts/install_goss.sh
@@ -9,7 +9,7 @@
 GOSS_VERSION=v0.3.6
 GOSS_SHA256=53dd1156ab66f2c4275fd847372e6329d895cfb2f0bcbec5f86c1c4df7236dde
 
-curl -L https://github.com/aelsabbahy/goss/releases/download/${GOSS_VERSION}/goss-linux-amd64 -o /usr/local/bin/goss
+curl -fL https://github.com/aelsabbahy/goss/releases/download/${GOSS_VERSION}/goss-linux-amd64 -o /usr/local/bin/goss
 
 # NOTE: extra whitespace between SHA sum and location is intentional, required for Alpine
 echo "${GOSS_SHA256}  /usr/local/bin/goss" | sha256sum -c - 2>&1 | grep OK
--- container/root/scripts/install_s6.sh
+++ container/root/scripts/install_s6.sh
@@ -9,8 +9,8 @@
 S6_NAME=s6-overlay-amd64.tar.gz
 S6_VERSION=v1.21.7.0
 
-curl -L https://github.com/just-containers/s6-overlay/releases/download/${S6_VERSION}/${S6_NAME} -o /tmp/${S6_NAME}
-curl -L https://github.com/just-containers/s6-overlay/releases/download/${S6_VERSION}/${S6_NAME}.sig -o /tmp/${S6_NAME}.sig
+curl -fL https://github.com/just-containers/s6-overlay/releases/download/${S6_VERSION}/${S6_NAME} -o /tmp/${S6_NAME}
+curl -fL https://github.com/just-containers/s6-overlay/releases/download/${S6_VERSION}/${S6_NAME}.sig -o /tmp/${S6_NAME}.sig
 curl https://keybase.io/justcontainers/key.asc| gpg --no-tty --batch --import
 gpg --no-tty --batch --verify /tmp/${S6_NAME}.sig /tmp/${S6_NAME}
 

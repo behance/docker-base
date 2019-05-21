a52d9d301c67a2cc2e0897392e1ba865ed5f0d6e
Bryan Latten <latten@adobe.com>
2019-05-21 15:37:34 -0400

    Dockerfile: S6 updated, duplication removed (#36)
    * Dockerfile: deduplicated S6 + goss installations
    
    - Converted S6 verification to gpg
    
    * Dockerfile: removed unsupported Ubuntu 17.10
    
    Canonical stopped support in July 2018
    
    * Travis: removing unsupported Ubuntu 17.10
    
    * Centos: special handling for folder symlinks
    
    * Scripts: added -f flag for curl

---
+++ container/root/scripts/install_s6.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+# Add S6 for zombie reaping, boot-time coordination, signal transformation/distribution
+# @see https://github.com/just-containers/s6-overlay
+#
+# Downloads, verifies, and extracts
+# Requires curl, gpg (or gnupg on Alpine), and tar to be present
+
+S6_NAME=s6-overlay-amd64.tar.gz
+S6_VERSION=v1.21.7.0
+
+curl -fL https://github.com/just-containers/s6-overlay/releases/download/${S6_VERSION}/${S6_NAME} -o /tmp/${S6_NAME}
+curl -fL https://github.com/just-containers/s6-overlay/releases/download/${S6_VERSION}/${S6_NAME}.sig -o /tmp/${S6_NAME}.sig
+curl https://keybase.io/justcontainers/key.asc| gpg --no-tty --batch --import
+gpg --no-tty --batch --verify /tmp/${S6_NAME}.sig /tmp/${S6_NAME}
+
+# Special handling - CentOS
+# @see https://github.com/just-containers/s6-overlay#bin-and-sbin-are-symlinks
+if [ -f /etc/centos-release ]; then
+  tar xzf /tmp/${S6_NAME} -C / --exclude="./bin"
+  tar xzf /tmp/${S6_NAME} -C /usr ./bin
+else
+  tar xzf /tmp/${S6_NAME} -C /
+fi
+
+rm /tmp/${S6_NAME} && rm /tmp/${S6_NAME}.sig

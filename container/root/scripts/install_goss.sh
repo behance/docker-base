5cc09813722ce4fef2e41023cc8b35367ce15adb
Bryan Latten <latten@adobe.com>
2019-06-24 12:56:50 -0400

    Goss: updates to v0.3.7

--- container/root/scripts/install_goss.sh
+++ container/root/scripts/install_goss.sh
@@ -6,8 +6,8 @@
 # Downloads, verifies, and installs
 # Requires curl and sha256sum to be present
 
-GOSS_VERSION=v0.3.6
-GOSS_SHA256=53dd1156ab66f2c4275fd847372e6329d895cfb2f0bcbec5f86c1c4df7236dde
+GOSS_VERSION=v0.3.7
+GOSS_SHA256=357f5c7f2e7949b412bce44349cd32ab19eb3947255a8ac805f884cc2c326059
 
 curl -fL https://github.com/aelsabbahy/goss/releases/download/${GOSS_VERSION}/goss-linux-amd64 -o /usr/local/bin/goss
 

64fd54cac2331420bc7de6fdefccd8c4854ab184
Bryan Latten <latten@adobe.com>
2019-12-10 10:51:24 -0500

    Goss: updated to 0.3.8 (#47)

--- container/root/scripts/install_goss.sh
+++ container/root/scripts/install_goss.sh
@@ -6,10 +6,13 @@
 # Downloads, verifies, and installs
 # Requires curl and sha256sum to be present
 
-GOSS_VERSION=v0.3.6
-GOSS_SHA256=53dd1156ab66f2c4275fd847372e6329d895cfb2f0bcbec5f86c1c4df7236dde
+GOSS_VERSION=v0.3.8
+GOSS_SHA256=4c82470543350371531f26f9d3b0265bff9e93d80d952f40cca212fc2f87864d
 
 curl -fL https://github.com/aelsabbahy/goss/releases/download/${GOSS_VERSION}/goss-linux-amd64 -o /usr/local/bin/goss
+
+curl -fL https://github.com/aelsabbahy/goss/releases/download/v0.3.8/goss-linux-amd64 -o /usr/local/bin/goss
+
 
 # NOTE: extra whitespace between SHA sum and location is intentional, required for Alpine
 echo "${GOSS_SHA256}  /usr/local/bin/goss" | sha256sum -c - 2>&1 | grep OK

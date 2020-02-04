bc49901ec6c2d64f4ebe6b5332ef023b5c710d1f
Bryan Latten <latten@adobe.com>
2020-02-04 17:05:49 -0500

    Goss: updated 0.3.9 Alpine: updated to 3.11

--- container/root/scripts/install_goss.sh
+++ container/root/scripts/install_goss.sh
@@ -6,13 +6,10 @@
 # Downloads, verifies, and installs
 # Requires curl and sha256sum to be present
 
-GOSS_VERSION=v0.3.8
-GOSS_SHA256=4c82470543350371531f26f9d3b0265bff9e93d80d952f40cca212fc2f87864d
+GOSS_VERSION=v0.3.9
+GOSS_SHA256=5e4a51a8c0f955e5ce99851b4a15eed9f1b3b6bee17f23dabda08071775663c8
 
 curl -fL https://github.com/aelsabbahy/goss/releases/download/${GOSS_VERSION}/goss-linux-amd64 -o /usr/local/bin/goss
-
-curl -fL https://github.com/aelsabbahy/goss/releases/download/v0.3.8/goss-linux-amd64 -o /usr/local/bin/goss
-
 
 # NOTE: extra whitespace between SHA sum and location is intentional, required for Alpine
 echo "${GOSS_SHA256}  /usr/local/bin/goss" | sha256sum -c - 2>&1 | grep OK

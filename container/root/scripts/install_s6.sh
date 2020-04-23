de3e526b15876171e94f3a932e58f57b3e1bedbd
Ko Uchiyama <ko-be@users.noreply.github.com>
2020-04-23 16:05:03 -0700

    Fix execlineb and failing tests (#52)

--- container/root/scripts/install_s6.sh
+++ container/root/scripts/install_s6.sh
@@ -16,7 +16,16 @@ gpg --no-tty --batch --verify /tmp/${S6_NAME}.sig /tmp/${S6_NAME}
 
 # Special handling - CentOS >= 7 + Ubuntu >= 20.04
 # @see https://github.com/just-containers/s6-overlay#bin-and-sbin-are-symlinks
-tar xzf /tmp/${S6_NAME} -C / --exclude="./bin"
-tar xzf /tmp/${S6_NAME} -C /usr ./bin
+# Need to also exclude the symlink included in s6-overlay-amd64.tar.gz as the symlink would otherwise overwrite the binary
+# $ tar tvzf s6-overlay-amd64.tar.gz |grep execlineb
+# -rwxr-xr-x root/root     33856 2019-03-21 12:29 ./bin/execlineb
+# lrwxrwxrwx root/root         0 2019-03-21 12:40 ./usr/bin/execlineb -> /bin/execlineb
+
+if [[ -L /bin ]]; then
+  tar xzf /tmp/${S6_NAME} -C / --exclude="./bin" --exclude="./usr/bin/execlineb"
+  tar xzf /tmp/${S6_NAME} -C /usr ./bin --exclude="./usr/bin/execlineb"
+else
+  tar xzf /tmp/${S6_NAME} -C /
+fi
 
 rm /tmp/${S6_NAME} && rm /tmp/${S6_NAME}.sig

e5450238e01a6f2d867fa5aab621b07fe66a7d80
Jan Michael Ong <jmong@adobe.com>
2022-08-28 12:03:31 -0700

    fix(ETHOS-38803): remove gpg validation. step fails with keyserver: EOF
    Notes:
    * Need to switch to newer version of s6-overlay which uses SHA256
      checksum validation instead

--- container/root/scripts/install_s6.sh
+++ container/root/scripts/install_s6.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-# Add S6 for zombie reaping, boot-time coordination, signal transformation/distribution
+# Add S6 for zombie reaping, boot-time coordination, signal transformation /
+# distribution
 # @see https://github.com/just-containers/s6-overlay
 #
 # Downloads, verifies, and extracts
@@ -11,26 +12,38 @@ ARCH="$(archstring --x64 amd64 --arm64 aarch64)"
 
 S6_NAME=s6-overlay-${ARCH}.tar.gz
 S6_VERSION=${S6_VERSION:="v2.1.0.2"}
-PUBLIC_KEY=6101B2783B2FD161
+# PUBLIC_KEY=6101B2783B2FD161
 
-curl -fL https://github.com/just-containers/s6-overlay/releases/download/${S6_VERSION}/${S6_NAME} -o /tmp/${S6_NAME}
-curl -fL https://github.com/just-containers/s6-overlay/releases/download/${S6_VERSION}/${S6_NAME}.sig -o /tmp/${S6_NAME}.sig
+S6_OVERLAY_BASE_URL=https://github.com/just-containers/s6-overlay/
 
-gpg --keyserver pgp.surfnet.nl --recv-keys $PUBLIC_KEY
-gpg --verify /tmp/${S6_NAME}.sig /tmp/${S6_NAME}
+curl -fL "${S6_OVERLAY_BASE_URL}releases/download/${S6_VERSION}/${S6_NAME}" \
+  -o "/tmp/${S6_NAME}"
+curl -fL "${S6_OVERLAY_BASE_URL}releases/download/${S6_VERSION}/${S6_NAME}.sig" \
+  -o "/tmp/${S6_NAME}.sig"
+
+# 2022-08-28: commented out since this always keeps failing will need to
+# revisit. With newer versions of s6-overlay, the way to validate is check
+# the checksum
+# @see https://github.com/just-containers/s6-overlay#verifying-downloads
+#gpg --import /tmp/s6-gpg-pub-key
+#gpg --keyserver pgp.surfnet.nl --recv-keys $PUBLIC_KEY
+#gpg --verify "/tmp/${S6_NAME}.sig" "/tmp/${S6_NAME}"
 
 # Special handling - CentOS >= 7 + Ubuntu >= 20.04
 # @see https://github.com/just-containers/s6-overlay#bin-and-sbin-are-symlinks
-# Need to also exclude the symlink included in s6-overlay-amd64.tar.gz as the symlink would otherwise overwrite the binary
+# Need to also exclude the symlink included in s6-overlay-amd64.tar.gz as the
+# symlink would otherwise overwrite the binary
 # $ tar tvzf s6-overlay-amd64.tar.gz |grep execlineb
 # -rwxr-xr-x root/root     33856 2019-03-21 12:29 ./bin/execlineb
 # lrwxrwxrwx root/root         0 2019-03-21 12:40 ./usr/bin/execlineb -> /bin/execlineb
 
 if [[ -L /bin ]]; then
-  tar xzf /tmp/${S6_NAME} -C / --exclude="./bin" --exclude="./usr/bin/execlineb"
-  tar xzf /tmp/${S6_NAME} -C /usr ./bin --exclude="./usr/bin/execlineb"
+  tar xzf "/tmp/${S6_NAME}" -C \
+    / --exclude="./bin" --exclude="./usr/bin/execlineb"
+  tar xzf "/tmp/${S6_NAME}" -C \
+    /usr ./bin --exclude="./usr/bin/execlineb"
 else
-  tar xzf /tmp/${S6_NAME} -C /
+  tar xzf "/tmp/${S6_NAME}" -C /
 fi
 
-rm /tmp/${S6_NAME} && rm /tmp/${S6_NAME}.sig
+rm "/tmp/${S6_NAME}" && rm "/tmp/${S6_NAME}.sig"
